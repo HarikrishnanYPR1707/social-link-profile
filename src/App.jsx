63c54a62b96e2a2104158611880e24fab730eba4
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-01-27 14:47:50 +0530

    Added tailwindcss and prettier-plugin-tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <h1 className="text-3xl font-bold text-red-400 underline">Hello world!</h1>
+  );
 };
 
 export default App;
+
+// Jessica Randall London, United Kingdom "Front-end developer and avid reader." GitHub Frontend Mentor LinkedIn Twitter Instagram
