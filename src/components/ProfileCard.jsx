a9ef2c0c88aa9c387f2f756f15b68ca9a4c899ae
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-01-27 15:06:34 +0530

    added image

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const ProfileCard = () => {
-  return <div className="bg-fmDarkGrey w-full">slkjdflkj</div>;
+  return (
+    <div className="bg-fmDarkGrey w-full">
+      {/* upper container  */}
+      <div className="">
+        <div className="">
+          <img src="./avatar-jessica.jpeg" alt="" />
+        </div>
+      </div>
+      {/* lower container  */}
+      <div className=""></div>
+    </div>
+  );
 };
 
 export default ProfileCard;
